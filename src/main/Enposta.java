44197ff6df790b03e300723ded336fc4b3e9536c
seojungwon <garden715@gmail.com>
2016-10-02 14:23:49 +0900

    edit sample speech level

--- src/main/Enposta.java
+++ src/main/Enposta.java
@@ -1,3 +1,4 @@
+package main;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,20 +15,21 @@ public class Enposta {
 		
 		Analyzer wordAnlyzer = new Analyzer();
 		BufferedReader br;
+		
 		try {
 			br = new BufferedReader(new FileReader("scriptset/a.txt"));
+			SpeechInfo curruntSpeech = new SpeechInfo("a.txt");
+			
 			while (true) {
 				String line = br.readLine();
 				if (line == null)
 					break;
+				
 				List<String> resultList = posta.analyze(line);
-				for (String result : resultList) {
-					String posTag = result.split("/")[1];
-					if (posTag.equals("VB") || posTag.equals("NN")) {
-						System.out.println(result);
-					}
-				}
+				curruntSpeech = wordAnlyzer.classifyingScript(resultList,curruntSpeech);
 			}
+			
+			System.out.println(curruntSpeech.level);
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
